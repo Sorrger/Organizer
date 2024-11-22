24a45a6c70eee160cc0ebe43f02b7818933ef876
Sorrger <Sorrger@gmail.com>
2024-11-22 16:44:31 +0100

    refactored git repo wihtout each lab folder, changed jlist to jtable, autoincrementing id of tasks

--- Labs/Lab1/src/main/java/polsl/pl/model/OutOfRangePriority.java
+++
@@ -1,7 +0,0 @@
-package polsl.pl.model;
-
-public class OutOfRangePriority extends RuntimeException {
-    public OutOfRangePriority(String message) {
-        super(message);
-    }
-}
--- Labs/Lab1/src/main/java/polsl/pl/model/Priority.java
+++
@@ -1,5 +0,0 @@
-package polsl.pl.model;
-
-public enum Priority {
-    LOW, MEDIUM, HIGH;
-}
